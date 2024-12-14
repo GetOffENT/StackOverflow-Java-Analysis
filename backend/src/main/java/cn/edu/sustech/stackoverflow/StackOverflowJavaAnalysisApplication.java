90ea3186a00ff89384fc7462f77116202a8ad4a5
Yuxian Wu <12212614@mail.sustech.edu.cn>
2024-12-14 19:18:41 +0800

    整合swagger3接口

--- backend/src/main/java/cn/edu/sustech/stackoverflow/StackOverflowJavaAnalysisApplication.java
+++ backend/src/main/java/cn/edu/sustech/stackoverflow/StackOverflowJavaAnalysisApplication.java
@@ -1,17 +1,33 @@
 package cn.edu.sustech.stackoverflow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("cn.edu.sustech.stackoverflow.mapper")
 @EnableTransactionManagement
+@Slf4j
 public class StackOverflowJavaAnalysisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StackOverflowJavaAnalysisApplication.class, args);
+        ConfigurableEnvironment environment = SpringApplication.run(StackOverflowJavaAnalysisApplication.class, args).getEnvironment();
+        log.info("""
+
+                        ----------------------------------------------------------
+                        \t\
+                        Application: '{}' is running Success!\s
+                        \t\
+                        Local URL: \thttp://localhost:{}
+                        \t\
+                        Document:\thttp://localhost:{}/doc.html
+                        ----------------------------------------------------------""",
+                environment.getProperty("spring.application.name"),
+                environment.getProperty("server.port"),
+                environment.getProperty("server.port"));
     }
 
 }
